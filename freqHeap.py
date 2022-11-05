db0ebf77d82b69a07e856ec144a6ed9cfedf442e
Shantala Gaitonde <shantalagaitonde@Shantalas-MacBook-Pro.local>
2022-11-05 12:50:51 -0400

    Have written code to print the huffman tree values and generate huffman code.

---
+++ freqHeap.py
@@ -0,0 +1,52 @@
+import heapq
+import frequency_list
+from node import Node
+from huffmanTree import HuffmanTree
+
+text = "aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeeffffffffffffffffffffffffffffffffffffffffffffff"
+letter_freq = frequency_list.generate_frequency_dictionary(text)
+print(letter_freq)
+li = []
+heapq.heapify(li)
+for value in letter_freq.values():
+    heapq.heappush(li, value)
+print(li)
+root = li[0].generate_huffmantree(li)
+print(root)
+
+
+def print_nodes(root):
+    if root.left is not None:
+        print_nodes(root.left)
+    if root.right is not None:
+        print_nodes(root.right)
+    print({root.get_frequency()}, {root.get_data()})
+
+def generate_codes(root, str1):
+    if root.left is not None:
+        str1 = str1 + "0"
+        generate_codes(root.left, str1)
+    if root.right is not None:
+        str1 = str1 + "1"
+        generate_codes(root.right, str1)
+    if root.get_data() is not None:
+        print({root.get_frequency()}, {root.get_data()}, str1)
+        str1 = ""
+
+str1 = ""
+print_nodes(root)
+generate_codes(root,str1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
